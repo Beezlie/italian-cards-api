0948d37bfc100c95ffe58d828211efa06849d12e
MattD <matthew.d.dias@gmail.com>
2021-09-27 09:52:57 -0400

    Remove build folder from source control and add to gitignore

--- build/middlewares/index.js
+++
@@ -1,3 +0,0 @@
-export { default as authenticated, verifyToken } from './authenticated.js';
-export { handleError, ErrorHandler } from './error.js';
-//# sourceMappingURL=index.js.map
--- build/socket/index.js
+++
@@ -1,2 +0,0 @@
-export { default as socket } from './socketController.js';
-//# sourceMappingURL=index.js.map
